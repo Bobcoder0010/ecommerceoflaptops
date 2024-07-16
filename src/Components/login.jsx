c76af81527c96e1b17cab69ee945f0e0ffa9073f
Bobcoder0010 <dellizulter@gmail.com>
2024-07-16 03:17:04 -0500

    first commit

---
+++ src/Components/login.jsx
@@ -0,0 +1,8 @@
+import react from "react";
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Bobcoder0010/ecommerceoflaptops.git
+git push -u origin main
